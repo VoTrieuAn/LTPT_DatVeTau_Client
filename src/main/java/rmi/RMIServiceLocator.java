099ed18d780b508443d9f27750a0c5bcb352822d
VoTrieuAn <voan2068@gmail.com>
2025-04-24 22:44:27 +0700

    Client NhanVien, HoaDon, TaiKhoan

--- src/main/java/rmi/RMIServiceLocator.java
+++ src/main/java/rmi/RMIServiceLocator.java
@@ -13,23 +13,11 @@ public class RMIServiceLocator {
     private static final String HOST = "localhost"; // hoặc IP như "192.168.1.10"
     private static final int PORT = 1099;
 
-//    public static BanVeServiceRemote getBanVeService() {
-//        return safeLookup("BanVeService");
-//    }
-
-//    public static TimVeServiceRemote getTimVeService() {
-//        return safeLookup("TimVeService");
-//    }
-//        context.bind("rmi://localhost:1099/VeService", veService);
-//        context.bind("rmi://localhost:1099/HanhKhacService", hanhKhachService);
-//        context.bind("rmi://localhost:1099/NhanVienService", nhanVienService);
-//        context.bind("rmi://localhost:1099/HoaDonService", hoaDonService);
-//        context.bind("rmi://localhost:1099/TaiKhoanService", taiKhoanService);
     public static VeService getVeService() {
         return safeLookup("VeService");
     }
     public static HanhKhachService getHanhKhachService() {
-        return safeLookup("HanhKhachService");
+        return safeLookup("HanhKhacService");
     }
     public static NhanVienService getNhanVienService() {
         return safeLookup("NhanVienService");
@@ -37,13 +25,9 @@ public class RMIServiceLocator {
     public static HoaDonService getHoaDonService() {
         return safeLookup("HoaDonService");
     }
-
     public static TaiKhoanService getTaiKhoanService() {
         return safeLookup("TaiKhoanService");
     }
-//    public static KhuyenMaiServiceRemote getKhuyenMaiService() {
-//        return safeLookup("KhuyenMaiService");
-//    }
 
     @SuppressWarnings("unchecked")
     private static <T> T safeLookup(String serviceName) {
